5b16dd151f98ff20684f9d6391ac8ab8e2e7938c
youssefHaddouch“git config --global user.email eparis@atlassian.comgit config --global user.name youssefHaddouch“ <youssef.aithaddouch@gmail.com>
2021-01-06 16:21:26 +0100

    check automatic changes from pull 2

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello best World\n')
+   response.end('Hello best fucking World\n')
 }).listen(3000)
 
 // Console will print the message
